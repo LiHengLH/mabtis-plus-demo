6bf7042c31fa3ebc2002fe52e269028eb87b5ede
V <1355202899@qq.com>
2019-01-05 16:36:58 +0800

    HAODE BA

--- mybatisplus-spring-boot/src/main/java/com/baomidou/springboot/controller/TestController.java
+++ mybatisplus-spring-boot/src/main/java/com/baomidou/springboot/controller/TestController.java
@@ -6,4 +6,11 @@ public class TestController {
 	public static void main(String[] args) {
 		System.out.println(111111);
 	}
+	
+	public  void test(){
+		System.out.println(22222);
+	}
+	public  void test2(){
+		System.out.println(3333);
+	}
 }
